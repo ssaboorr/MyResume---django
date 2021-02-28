4b558dc906536a67b6c037c730edcf39270050ce
ssaboorr <sabooransari777@gmail.com>
2021-02-28 22:40:46 +0530

    added css and js

--- Myresume/Myresume/settings.py
+++ Myresume/Myresume/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR = BASE_DIR / 'static'
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR]
+
--- Myresume/education/views.py
+++ Myresume/education/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def edu(request):
+    return render(request, 'education/education.html')
